f2b2f6a2a786ced716a6b5cb076b0c4256b27ffc
WindArrow <wind.dong.dream@gmail.com>
2016-08-18 16:35:35 +0900

    upload_images

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.post('/upload_profile_image', function(req, res) {
 });
 
 app.post('/upload_images', function(req, res) {
-    console.log('req = ' + req);
+    console.log('req = ' + JSON.stringify(req));
     console.log('req.files = ' + req.files);
     fs.readFile(req.files.uploadFile.path, function(err, data) {
         if (err) {
